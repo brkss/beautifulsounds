8bbae4c4ab58b319d819f9ab9a21580f3c77c3bb
Brahim Berkass <bberkass@Driouchs-MacBook-Air.local>
2022-02-21 23:39:07 +0100

    🦤 change volume on slide git add .

--- components/SoundBox.tsx
+++ components/SoundBox.tsx
@@ -7,6 +7,7 @@ import { Volume } from './Volume';
 export const SoundBox : React.FC = () => {
 
     const [status, SetStatus] = React.useState('PAUSED');
+    const [volume, SetVolume] = React.useState(0);
 
     const handleStatus = () => {
         if(status == 'PAUSED')
@@ -15,13 +16,21 @@ export const SoundBox : React.FC = () => {
             SetStatus('PAUSED');
     }
 
+    const handleVolumeChange = (v: number) => {
+        if(v <= 0)
+            SetStatus('PAUSED'); 
+        else
+            SetStatus('PLAYING'); 
+        SetVolume(v);
+    }
+
     return(
-        <Center onClick={() => handleStatus()} borderRadius={'15px'} bg={status == 'PAUSED' ? '#f7f4f4' : '#d4f5ce'} height={'100%'}>
+        <Center borderRadius={'15px'} bg={status == 'PAUSED' ? '#f7f4f4' : '#d4f5ce'} height={'100%'}>
             <Box w={'100%'} p={'10px'}>
             <BsCloudRain size={30} style={{margin: 'auto', marginBottom: '10px'}} />
-            <Sound playStatus={status as any} url={'/sounds/rain-03.mp3'} onLoading={() => console.log("Loading !")}
+            <Sound playStatus={status as any} volume={volume} url={'/sounds/rain-03.mp3'} onLoading={() => console.log("Loading !")}
       onPlaying={() => console.log("playing sound !")} />
-            <Volume />
+            <Volume change={(v) => handleVolumeChange(v)} />
             </Box>
         </Center>
     );
--- components/Volume.tsx
+++ components/Volume.tsx
@@ -1,20 +1,28 @@
 import { Slider, SliderMark, SliderTrack, SliderFilledTrack, Tooltip, SliderThumb } from '@chakra-ui/react';
 import React from 'react';
 
+interface Props {
+    change: (val: number) => void; 
+}
 
-export const Volume : React.FC = () => {
+export const Volume : React.FC<Props> = ({change}) => {
 
     const [sliderValue, setSliderValue] = React.useState(5)
     const [showTooltip, setShowTooltip] = React.useState(false)
-    return(
+    
+    const handleVolumeChange = (v: number) => {
+        setSliderValue(v);
+        change(v);
+    }
 
+    return(
     <Slider
       id='slider'
       defaultValue={5}
       min={0}
       max={100}
       colorScheme='blackAlpha'
-      onChange={(v) => setSliderValue(v)}
+      onChange={(v) => handleVolumeChange(v)}
       onMouseEnter={() => setShowTooltip(true)}
       onMouseLeave={() => setShowTooltip(false)}
     >
