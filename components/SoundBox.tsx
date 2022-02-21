7d59b2409a9ff22988536079636aef6f0bcde841
Brahim Berkass <bberkass@Driouchs-MacBook-Air.local>
2022-02-21 23:19:03 +0100

    🦇 Playing Sounds !

--- components/SoundBox.tsx
+++ components/SoundBox.tsx
@@ -3,15 +3,23 @@ import React from 'react'
 import { Center } from '@chakra-ui/react';
 import { BsCloudRain } from 'react-icons/bs';
 import Sound from 'react-sound';
+//import RainSound from "../public/sound/rain-03.mp3"
 
 export const SoundBox : React.FC = () => {
 
     const [status, SetStatus] = React.useState('PAUSED');
 
+    const handleStatus = () => {
+        if(status == 'PAUSED')
+            SetStatus('PLAYING');
+        else
+            SetStatus('PAUSED');
+    }
+
     return(
-        <Center onClick={() => {SetStatus('PLAYING')}} height={'100%'}>
+        <Center onClick={() => handleStatus()} height={'100%'}>
             <BsCloudRain />
-            <Sound playStatus={status as any} url={'/rain-03.mp3'} onLoading={() => console.log("Loading !")}
+            <Sound playStatus={status as any} url={'/sounds/rain-03.mp3'} onLoading={() => console.log("Loading !")}
       onPlaying={() => console.log("playing sound !")} />
         </Center>
     );
--- next.config.js
+++ next.config.js
@@ -3,4 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+module.exports = {
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "sounds/[path][name].[hash][ext]",
+      },
+    });
+
+    return config;
+  },
+};
+
 module.exports = nextConfig
